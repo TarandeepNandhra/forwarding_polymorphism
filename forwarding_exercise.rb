a8aac2ea1d28e6c77a91b388ad9bad77a497e883
taran314 <tarandeep-nandhra@hotmail.co.uk>
2021-03-11 22:04:50 +0000

    Tested forwarding in irb - looks good!

--- forwarding_exercise.rb
+++ forwarding_exercise.rb
@@ -1,14 +1,24 @@
-class User
-  def initialize(name, bio, age, password)
-    @name = name
-    @bio = bio
-    @age = age
+class Authenticate
+  def initialize(password)
     @password = password
   end
 
   def authenticate(candidate_password)
     return true if candidate_password == @password
     false
+  end
+end
+
+class User
+  def initialize(name, bio, age, password)
+    @name = name
+    @bio = bio
+    @age = age
+    @security = Authenticate.new(password)
+  end
+
+  def authenticate(candidate_password)
+    @security.authenticate(candidate_password)
   end
 
   def profile
